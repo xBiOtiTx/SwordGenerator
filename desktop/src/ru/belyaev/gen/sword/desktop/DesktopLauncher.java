8fbb7a647258fc25e109a8ed7e01096bd169c3a9
Eugen <ieugenor@gmail.com>
2018-06-12 15:33:49 +0300

    INIT

---
+++ desktop/src/ru/belyaev/gen/sword/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.belyaev.gen.sword.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.belyaev.gen.sword.SwordGeneratorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SwordGeneratorGame(), config);
+	}
+}
